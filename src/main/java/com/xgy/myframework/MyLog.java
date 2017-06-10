76363f451ea1994699815ab3573b5fc415bf5f38
xie_gaoyang <xie_gaoyang@gowild.cn>
2017-06-10 20:57:45 +0800

    Server，Log

---
+++ src/main/java/com/xgy/myframework/MyLog.java
@@ -0,0 +1,29 @@
+package com.xgy.myframework;
+
+/**
+ * Created by gowild.cn on 2017/6/10.
+ */
+public class MyLog {
+
+    private MyLog() {}
+
+    private static MyLog myLog = null;
+
+    public static MyLog getInstance() {
+        if (null == myLog) {
+            synchronized (MyLog.class) {
+                if (null == myLog) {
+                    myLog = new MyLog();
+                }
+            }
+        }
+        return myLog;
+    }
+
+    public void info(String msg) {
+        System.out.println(msg);
+    }
+
+
+
+}
--- src/main/java/com/xgy/myframework/MyServer.java
+++ src/main/java/com/xgy/myframework/MyServer.java
@@ -1,7 +1,58 @@
 package com.xgy.myframework;
 
+import java.net.ServerSocket;
+
 /**
  * Created by gowild.cn on 2017/6/10.
  */
 public class MyServer {
+
+    private final String ip = "localhost";
+    private final int port = 12345;
+
+    private boolean isRunning = false;
+
+    private ServerSocket serverSocket = null;
+
+    public boolean init() {
+
+
+
+        return true;
+    }
+
+    public boolean uninit() {
+        return true;
+    }
+
+    public boolean start() {
+
+        while (isRunning) {
+
+        }
+
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+
+        MyServer myServer = new MyServer();
+
+        if (!myServer.init()) {
+            MyLog.getInstance().info("服务器初始化失败.");
+            return;
+        }
+
+        if (!myServer.start()) {
+            MyLog.getInstance().info("服务器初始化失败.");
+            return;
+        }
+
+        if (!myServer.uninit()) {
+            MyLog.getInstance().info("服务器反初始化失败.");
+        }
+
+    }
+
 }
